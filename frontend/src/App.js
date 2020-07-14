add5189bacd89b81146a71bb6bcfef8ccfec3eb7
juandavid716 <jbojato@uninorte.edu.co>
2020-07-13 20:06:24 -0500

    components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation";
+import CreateNote from "./components/CreateNote";
+import NoteList from "./components/NoteList";
+import CreateUser from "./components/CreateUser";
 function App() {
-  return <div className="App">Hola</div>;
+  return (
+    <Router>
+      <Navigation />
+      <Route path="/" exact component={NoteList} />
+      <Route path="/edit/:id" exact component={CreateNote} />
+      <Route path="/create" exact component={CreateNote} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;
